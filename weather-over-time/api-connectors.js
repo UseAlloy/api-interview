e74e19fc86fd0fcc7769c08db0b550195fdbdbe7
Erik Larsen <erik.w.larsen@gmail.com>
2020-03-04 13:16:03 -0500

    interview changes

--- weather-over-time/api-connectors.js
+++ weather-over-time/api-connectors.js
@@ -7,7 +7,30 @@ const mapWeatherPointTimes = (weatherPoints) => {
   }));
 };
 
-const transformDirectionsToWeatherPoints = (directions, interval) => {};
+const transformDirectionsToWeatherPoints = (directions, interval) => {
+  let timeTaken = 0;
+  let locations = [{
+    lat: directions[0].startPoint.lat,
+    lon: directions[0].startPoint.lng
+  }];
+  for (let dir of directions) {
+    timeTaken += dir.time;
+    if (timeTaken > 60 * 60) {
+      locations.push({
+        lat: dir.startPoint.lat,
+        lon: dir.startPoint.lng
+      });
+      timeTaken -= 60 * 60;
+    }
+  }
+  locations.push({
+    lat: directions[directions.length - 1].startPoint.lat,
+    lon: directions[directions.length - 1].startPoint.lng
+  });
+
+    console.log(locations);
+    return locations;
+};
 
 module.exports = {
   transformDirectionsToWeatherPoints,
--- weather-over-time/maps-api.js
+++ weather-over-time/maps-api.js
@@ -1,5 +1,15 @@
+const request = require('request-promise');
 const apiKey = process.env.MAPQUEST_API_KEY;
 
-const getDirections = async (start, end, mode) => {};
+const getDirections = async (start, end, mode) => {
+  let url = 'http://www.mapquestapi.com/directions/v2/route?';
+  url += 'key=' + apiKey + '&from=' + start + '&to=' + end + '&routeType=' + mode;
+  let data = await request(url, {json: true})
+    .then(data => {
+      return data.route.legs[0].maneuvers;
+    })
+
+    return data;
+};
 
 module.exports = { getDirections };
